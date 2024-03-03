adb436b30e4aa4b55b41a94666a3103244045d12
Nitish2004kumar <149707170+Nitish2004kumar@users.noreply.github.com>
2024-03-03 06:48:30 +0000

    javascript basic done

---
+++ Basic/dataType.js
@@ -0,0 +1,20 @@
+// "use strict" treat all js code as newer version
+// alert(3+3)//we are using node js not browser
+/*
+DATA TYPE
+1.number =>2 to power 53
+2.bigint
+3.string=> ""
+4.boolean
+5.null it shows empty data type
+6.undefined
+7.symbol =>unique
+8.object
+
+
+*/
+
+console.log(typeof"nitish");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
---
+++ Basic/someProb.js
@@ -0,0 +1,26 @@
+
+const accountId =13242
+let accountEmail = "nitishlumar@google.com"
+var  accountPassword = "1234"
+let accountState;
+accountCity = "chennai"
+accountEmail = "nitis@go.com"
+accountPassword="23423"
+accountCity="Bihar"
+
+
+// accountId = 2 not allowed
+
+/*
+prefer not to use var because use issue in block scope and funcitional scope
+
+*/
+console.log(accountEmail);
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+
+
